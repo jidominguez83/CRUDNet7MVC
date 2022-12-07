330152ecd274a20039fe967ef7d429b23b4eee11
jidominguez83 <domingueztorres.ji@gmail.com>
2022-12-06 20:58:32 -0600

    Agregar archivos de proyecto.

---
+++ CRUDNet7MVC/Controllers/Datos/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDNet7MVC.Controllers.Datos
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        //Agregar aquí los modelos
+    }
+}
