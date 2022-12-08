0450b6a5b1351ed04f40f68a9412e699480e9558
Jesús Iván Domínguez Torres <jdominguez@seslp.gob.mx>
2022-12-08 13:50:07 -0600

    Se modificó la maquetación, se agragaron las funcionalidades de listar los registros, editarlos, verlos individualmente y borrarlos.

--- CRUDNet7MVC/Controllers/InicioController.cs
+++ CRUDNet7MVC/Controllers/InicioController.cs
@@ -21,9 +21,109 @@ namespace CRUDNet7MVC.Controllers
             return View(await _contexto.Contacto.ToListAsync());
         }
 
-        public IActionResult Privacy()
+
+        [HttpGet]
+        public IActionResult Crear()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Contacto contacto)
+        {
+            if(ModelState.IsValid)
+            {
+                _contexto.Contacto.Add(contacto);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+            if(contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Contacto.Update(contacto);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Detalle(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+
+        [HttpGet]
+        public IActionResult Borrar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpPost, ActionName("Borrar")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> BorrarContacto(int? id)
+        {
+            var contacto = await _contexto.Contacto.FindAsync(id);
+
+            if(contacto == null)
+            {
+                return View();
+            }
+
+            // Borrado
+            _contexto.Contacto.Remove(contacto);
+            await _contexto.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
